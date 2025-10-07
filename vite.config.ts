06ad85e1fcc54e71f0fd3e41a6d424ab28ce27bd
ethosexplorer <ethosexplorer@gmail.com>
2025-10-07 07:43:34 +0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  server: {
+    port: 5173, // Use Vite's default port
+    proxy: {
+      '/api': {
+        target: 'https://goeditore.com',
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
+})
