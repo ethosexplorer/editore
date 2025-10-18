a2695dd762ba869502c3fa938f7434844c643c14
ethosexplorer <ethosexplorer@gmail.com>
2025-10-19 01:05:24 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173, // Use Vite's default port
     proxy: {
       '/api': {
-        target: 'http://localhost:3000/',
+        target: 'https://goeditore.com/',
         changeOrigin: true,
         secure: false,
       }
