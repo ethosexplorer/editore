e3f2da6693251b7c6718a17490648ed38b42184e
ethosexplorer <ethosexplorer@gmail.com>
2025-10-19 01:03:35 +0500

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 5173, // Use Vite's default port
     proxy: {
       '/api': {
-        target: 'https://goeditore.com',
+        target: 'http://localhost:3000/',
         changeOrigin: true,
         secure: false,
       }
